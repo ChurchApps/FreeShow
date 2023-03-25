2ccb514e57f851a023c0944fcd7aa5952bf07384
Kristoffer Vassbø <kristoffervassbo@gmail.com>
2023-03-25 18:47:10 +0100

    🚩 Added Italian language

--- src/frontend/utils/languageData.ts
+++ src/frontend/utils/languageData.ts
@@ -1,22 +1,24 @@
 export const languages: any = {
-  // language identifier: language name in original language
-  en: "English",
-  no: "Norsk",
-  sk: "Slovenčina",
-  es: "Español",
-  pt_BR: "Português do Brasil",
-  de: "Deutsch",
+    // language identifier: language name in original language
+    en: "English",
+    no: "Norsk",
+    sk: "Slovenčina",
+    es: "Español",
+    pt_BR: "Português do Brasil",
+    de: "Deutsch",
+    it: "Italiano",
 }
 
 // Help: https://developers.google.com/search/docs/advanced/crawling/localized-versions#language-codes
 // Language identifier: https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
 // Region: https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2
 export const replace: any = {
-  // language identifier: browser language/region code
-  en: ["en-US", "en-GB", "en-AU"],
-  no: ["nb", "nn"],
-  sk: ["sk"],
-  es: ["es"],
-  pt_BR: ["pt-BR"],
-  de: ["de"],
+    // language identifier: browser language/region code
+    en: ["en-US", "en-GB", "en-AU"],
+    no: ["nb", "nn"],
+    sk: ["sk"],
+    es: ["es"],
+    pt_BR: ["pt-BR"],
+    de: ["de"],
+    it: ["it"],
 }
