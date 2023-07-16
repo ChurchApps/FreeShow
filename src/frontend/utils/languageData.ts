09c33548404ac8638ac8c68437f9f3555ba66741
Filip Ilić <filip@filipilic.com>
2023-07-16 13:05:29 +0200

    Added Serbian translation

--- src/frontend/utils/languageData.ts
+++ src/frontend/utils/languageData.ts
@@ -7,7 +7,8 @@ export const languages: any = {
     pt_BR: "Português do Brasil",
     de: "Deutsch",
     it: "Italiano",
-    pl: "Polski"
+    pl: "Polski",
+    sr: "Srpski"
 }
 
 // Help: https://developers.google.com/search/docs/advanced/crawling/localized-versions#language-codes
@@ -22,5 +23,6 @@ export const replace: any = {
     pt_BR: ["pt-BR"],
     de: ["de"],
     it: ["it"],
-    pl: ["pl"]
+    pl: ["pl"],
+    sr: ["sr"]
 }
