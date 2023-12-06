0027cdafa4113cab382ed9a6ee5447f29e627ddb
Jeremy Zongker <jeremy@zongker.net>
2023-12-06 16:01:33 -0600

    Updated gyp and notarize packages

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -1,7 +1,7 @@
 // From https://github.com/simonw/til/blob/main/electron/sign-notarize-electron-macos.md
 // Based on https://kilianvalkhof.com/2019/electron/notarizing-your-electron-application/
 
-const { notarize } = require("electron-notarize")
+const { notarize } = require("@electron/notarize")
 
 exports.default = async function notarizing(context) {
     const { electronPlatformName, appOutDir } = context
