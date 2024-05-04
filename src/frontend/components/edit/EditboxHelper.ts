68eaa72fb6a7e753a40070f38e42f31aed4ae975
Jeremy Zongker <jeremy@zongker.net>
2024-05-04 03:07:31 -0500

    Improved method of determining last line changed

--- src/frontend/components/edit/EditboxHelper.ts
+++ src/frontend/components/edit/EditboxHelper.ts
@@ -1,24 +1,28 @@
 import type { Item, Line } from "../../../types/Show"
 
 export class EditboxHelper {
-  static determineCaretLine(item:Item, newLines: Line[]) {
-    let changeCount = 0
-    let lastChangedLine = -1
 
-    if (newLines.length !== item?.lines?.length) {
-        for (let i = 0; i < newLines.length; i++) {
-            let oldVal = "(blank)"
-            if (item?.lines && item.lines.length > i) oldVal = item.lines[i].text[0].value
-            console.log("COMPARING", i, newLines[i].text[0].value, "**", oldVal)
-            if (newLines[i].text[0].value !== oldVal) {
-                changeCount++
-                lastChangedLine = i
-                console.log("CHANGED LINE", i, newLines[i].text[0].value, "**", oldVal)
-                if (changeCount > 1) break
-            }
-        }
+  static determineCaretLine(item:Item, newLines: Line[]) {
+    const oldTexts:string[] = [];
+    const newTexts:string[] = [];
+
+    item.lines?.forEach((line) => {
+      oldTexts.push(line.text[0].value);
+    });
+
+    newLines.forEach((line) => {
+      newTexts.push(line.text[0].value);
+    });
+
+    let lastLineChanged = -1;
+    for (let i=0; i<newTexts.length; i++) {
+      const nt = newTexts[i];
+      const index = oldTexts.indexOf(nt);
+      if (index === -1) lastLineChanged = i;
+      else oldTexts.splice(index, 1);
     }
-    return lastChangedLine
-}
+    return lastLineChanged;
+  }
+
 
 }
