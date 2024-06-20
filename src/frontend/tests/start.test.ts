11ceeccd1e3019d9a8cef06f825798d9b594eb47
Daniel Ip <kg4368@yahoo.com.hk>
2024-06-20 11:17:55 +0100

    Add a screenshot around assert

--- src/frontend/tests/start.test.ts
+++ src/frontend/tests/start.test.ts
@@ -22,51 +22,55 @@ test("Launch electron app", async () => {
 
     // Get the first window that the app opens, wait if necessary.
     const window = await electronApp.firstWindow()
-
-    // Print the title.
-    console.log(await window.title())
-
-    // Capture a screenshot.
-    // await window.screenshot({ path: "intro.png" })
-
-    // Direct Electron console to Node terminal.
-    window.on("console", console.log)
-
-    // Initial setup
-    await window.getByText("Get Started!").click({ timeout: 1000 })
-    await window.getByTestId("alert-ack").click({ timeout: 1000 })
-
-    // Create a new project, then try creating a new show under the project
-    await window.locator("#leftPanel").getByText("New project").click({ timeout: 1000 })
-    await window.getByText("New show").first().click({ timeout: 1000 })
-
-    // Expect the pop up to be visible
-    await expect(window.getByText("Name")).toBeVisible({ timeout: 1000 })
-
-    // Fill name of show
-    await window.locator("#name").fill("New Test Show", { timeout: 1000 })
-
-    // Select category (this will sometimes not have any categories)
-    // await window.getByText("—").click()
-    // await window.locator("#id_categorysong").click()
-
-    // Put lyrics
-    await window.getByText("Quick Lyrics").click({ timeout: 1000 })
-    let lyricsBox = window.getByPlaceholder("[Verse]")
-    await lyricsBox.focus()
-    await lyricsBox.fill(`[Verse]\ntest line 1\ntest line 2\n\n[Chorus]\ntest line 3\ntest line 4`, { timeout: 1000 })
-
-    // Click new show
-    await window.getByTestId("create.show.popup.new.show").click({ timeout: 1000 })
-
-    // Try changing group for Chorus
-    await window.getByTitle("Chorus").click({ button: "right", timeout: 1000 })
-    await window.getByText("Change group").hover()
-    await window.getByText("Outro").click({ timeout: 1000 })
-
-    // Verify the group changing was successful
-    await expect(window.getByTitle("Outro")).toBeVisible({ timeout: 1000 })
-
-    // Close after finishing
-    await electronApp.close()
+    try {
+        // Print the title.
+        console.log(await window.title())
+    
+        // Capture a screenshot.
+        // await window.screenshot({ path: "intro.png" })
+    
+        // Direct Electron console to Node terminal.
+        window.on("console", console.log)
+    
+        // Initial setup
+        await window.getByText("Get Started!").click({ timeout: 1000 })
+        await window.getByTestId("alert-ack").click({ timeout: 1000 })
+    
+        // Create a new project, then try creating a new show under the project
+        await window.locator("#leftPanel").getByText("New project").click({ timeout: 1000 })
+        await window.getByText("New show").first().click({ timeout: 1000 })
+    
+        // Expect the pop up to be visible
+        await expect(window.getByText("Name")).toBeVisible({ timeout: 1000 })
+    
+        // Fill name of show
+        await window.locator("#name").fill("New Test Show", { timeout: 1000 })
+    
+        // Select category (this will sometimes not have any categories)
+        // await window.getByText("—").click()
+        // await window.locator("#id_categorysong").click()
+    
+        // Put lyrics
+        await window.getByText("Quick Lyrics").click({ timeout: 1000 })
+        let lyricsBox = window.getByPlaceholder("[Verse]")
+        await lyricsBox.focus()
+        await lyricsBox.fill(`[Verse]\ntest line 1\ntest line 2\n\n[Chorus]\ntest line 3\ntest line 4`, { timeout: 1000 })
+    
+        // Click new show
+        await window.getByTestId("create.show.popup.new.show").click({ timeout: 1000 })
+    
+        // Try changing group for Chorus
+        await window.getByTitle("Chorus").click({ button: "right", timeout: 1000 })
+        await window.getByText("Change group").hover({ timeout: 1000 })
+        await window.getByText("Outro").click({ timeout: 1000 })
+    
+        // Verify the group changing was successful
+        await expect(window.getByTitle("Outro")).toBeVisible({ timeout: 1000 })
+    
+        // Close after finishing
+        await electronApp.close()
+    } catch (ex) {
+        await window.screenshot({ path: "failed.png" })
+        throw ex
+    }
 })
