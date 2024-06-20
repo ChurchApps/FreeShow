c87f34298487ee61741333bb6066b367d56a51f8
Daniel Ip <kg4368@yahoo.com.hk>
2024-06-20 10:26:13 +0100

    Add timeout to actions

--- src/frontend/tests/start.test.ts
+++ src/frontend/tests/start.test.ts
@@ -33,39 +33,39 @@ test("Launch electron app", async () => {
     window.on("console", console.log)
 
     // Initial setup
-    await window.getByText("Get Started!").click()
-    await window.getByTestId("alert-ack").click()
+    await window.getByText("Get Started!").click({ timeout: 1000 })
+    await window.getByTestId("alert-ack").click({ timeout: 1000 })
 
     // Create a new project, then try creating a new show under the project
-    await window.locator("#leftPanel").getByText("New project").click()
-    await window.getByText("New show").first().click()
+    await window.locator("#leftPanel").getByText("New project").click({ timeout: 1000 })
+    await window.getByText("New show").first().click({ timeout: 1000 })
 
     // Expect the pop up to be visible
-    await expect(window.getByText("Name")).toBeVisible()
+    await expect(window.getByText("Name")).toBeVisible({ timeout: 1000 })
 
     // Fill name of show
-    await window.locator("#name").fill("New Test Show")
+    await window.locator("#name").fill("New Test Show", { timeout: 1000 })
 
     // Select category (this will sometimes not have any categories)
     // await window.getByText("—").click()
     // await window.locator("#id_categorysong").click()
 
     // Put lyrics
-    await window.getByText("Quick Lyrics").click()
+    await window.getByText("Quick Lyrics").click({ timeout: 1000 })
     let lyricsBox = window.getByPlaceholder("[Verse]")
     await lyricsBox.focus()
-    await lyricsBox.fill(`[Verse]\ntest line 1\ntest line 2\n\n[Chorus]\ntest line 3\ntest line 4`)
+    await lyricsBox.fill(`[Verse]\ntest line 1\ntest line 2\n\n[Chorus]\ntest line 3\ntest line 4`, { timeout: 1000 })
 
     // Click new show
-    await window.getByTestId("create.show.popup.new.show").click()
+    await window.getByTestId("create.show.popup.new.show").click({ timeout: 1000 })
 
     // Try changing group for Chorus
-    await window.getByTitle("Chorus").click({ button: "right" })
+    await window.getByTitle("Chorus").click({ button: "right", timeout: 1000 })
     await window.getByText("Change group").hover()
-    await window.getByText("Outro").click()
+    await window.getByText("Outro").click({ timeout: 1000 })
 
     // Verify the group changing was successful
-    await expect(window.getByTitle("Outro")).toBeVisible()
+    await expect(window.getByTitle("Outro")).toBeVisible({ timeout: 1000 })
 
     // Close after finishing
     await electronApp.close()
