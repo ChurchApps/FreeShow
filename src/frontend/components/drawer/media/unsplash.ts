7bba161c50611f16d0bd94678e6c987faa1d0ec9
Adriel Sand <61815862+thebiblelover7@users.noreply.github.com>
2024-08-06 07:59:03 +0000

    Initial support for Unsplash images (#713)
    * 🌄 Added initial support for Unsplash images
    
    * 🌄 Fixed description causing load failure
    
    * 🌄 Fixed Unsplash icon

---
+++ src/frontend/components/drawer/media/unsplash.ts
@@ -0,0 +1,39 @@
+import { getExtension } from "../../helpers/media"
+
+// https://unsplash.com/documentation
+
+let cache: any = {}
+
+export async function loadFromUnsplash(query: string = ""): Promise<any[]> {
+    return new Promise((resolve) => {
+        console.log(query)
+        if (cache[query]) return resolve(cache[query])
+
+        let url: string = "https://api.unsplash.com/search/photos/?client_id=CaXvP_plzuAivss1MbiwGU3-rXd3zZphCifOVplX6Cg&content_filter=high&per_page=30&query=" // this is still a demo key, so it should be changed before release
+        url += encodeURIComponent(query)
+        console.log(url)
+
+        let results: any = []
+        fetch(url)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log("UNSPLASH", data)
+
+
+                results = data.results.map((media) => {
+                    // previewURL
+                    let path = media.urls.regular
+                    return { path, name: media.alt_description, extension: getExtension(path) }
+                })
+
+                cache[query] = results
+
+                // let img = { path, favourite: a.favourite === true, name, extension: p.extension, audio: a.audio === true }
+                return resolve(results)
+            })
+            .catch((error) => {
+                console.log(error)
+                return resolve([])
+            })
+    })
+}
