6ff8e633f950b4c61d5a3ca4cc8a141943508468
Kristoffer <kristoffervassbo@gmail.com>
2024-12-24 11:30:49 +0100

    📺 Online videos saved in synced settings

--- src/electron/data/defaults.ts
+++ src/electron/data/defaults.ts
@@ -43,10 +43,6 @@ export const defaultSettings: { [key in SaveListSettings]: any } = {
     maxConnections: 10,
     mediaFolders: {},
     audioFolders: {},
-    playerVideos: {
-        chosen: { name: "The Chosen Trailer", type: "youtube", id: "X-AJdKty74M" },
-        jesus: { name: "Jesus, Only Jesus", type: "vimeo", id: "426363743" },
-    },
     resized: {
         leftPanel: 290,
         rightPanel: 290,
@@ -122,6 +118,10 @@ export const defaultSyncedSettings: { [key in SaveListSyncedSettings]: any } = {
     },
     groups: defaultGroups,
     midiIn: {},
+    playerVideos: {
+        chosen: { name: "The Chosen Trailer", type: "youtube", id: "X-AJdKty74M" },
+        jesus: { name: "Jesus, Only Jesus", type: "vimeo", id: "426363743" },
+    },
     videoMarkers: {},
     mediaTags: {},
     customizedIcons: { disabled: [], svg: [] },
