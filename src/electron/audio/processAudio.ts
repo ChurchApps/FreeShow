148898620c6f3eda45d0f9a211061cb2572e3140
Kristoffer <kristoffervassbo@gmail.com>
2025-02-27 15:38:00 +0100

    Updates

--- src/electron/audio/processAudio.ts
+++ src/electron/audio/processAudio.ts
@@ -1,20 +1,32 @@
-import { OpusEncoder } from "@discordjs/opus"
 import { NdiSender } from "../ndi/NdiSender"
 
 // const isStopping = false
 const channelCount = 2
 const sampleRate = 48000 // Hz
 
-const opusEncoder = new OpusEncoder(sampleRate, channelCount)
+let opusEncoder: any = null
+try {
+    const { OpusEncoder } = require("@discordjs/opus")
+    opusEncoder = new OpusEncoder(sampleRate, channelCount)
+} catch (err) {
+    console.log("OPUS not found!")
+}
 
 export async function processAudio(buffer: Buffer, timeDelay = 0) {
+    if (!opusEncoder) return
+
     /*  optionally delay the processing  */
     const offset = timeDelay
     if (offset > 0) await new Promise((resolve) => setTimeout(resolve, offset))
     // if (isStopping) return
 
     // decode raw OPUS packets into raw PCM/interleaved/signed-int16/little-endian data
-    buffer = opusEncoder.decode(buffer)
+    try {
+        buffer = opusEncoder.decode(buffer)
+    } catch (err) {
+        console.log("Could not process audio.")
+        return
+    }
 
     NdiSender.sendAudioBufferNDI(buffer, { sampleRate, channelCount })
     // Object.keys(NdiSender.NDI).forEach((id) => {
