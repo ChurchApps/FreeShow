9a46b32503405adf0cd9d8ba4ab28899873c7efe
Jeremy Zongker <jeremy@zongker.net>
2025-05-14 17:26:39 -0500

    Converted chums startup to a non-async method

--- src/electron/chums/index.ts
+++ src/electron/chums/index.ts
@@ -16,10 +16,12 @@ export function chumsLoadServices() {
   return ChumsImport.loadServices()
 }
 
-export async function chumsStartupLoad(scope: ChumsScopes = "plans") {
+export function chumsStartupLoad(scope: ChumsScopes = "plans") {
   if (!stores.ACCESS.get(`chums_${scope}`)) return
-  const connected = await ChumsConnect.connect(scope)
-  if (!connected) return
-  await ChumsExport.sendSongsToChums()
-  await ChumsImport.loadServices()
+  ChumsConnect.connect(scope).then(async (connected) => {
+    if (!connected) return
+    ChumsExport.sendSongsToChums()
+    ChumsImport.loadServices()
+  });
+
 }
