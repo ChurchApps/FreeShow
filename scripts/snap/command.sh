854f5d7bbfc2338f86550d0bb0e879380894b479
Erich Eickmeyer <erich@ericheickmeyer.com>
2025-08-23 09:11:02 -0700

    Update command.sh for new executable name
    Fixes #2021

--- scripts/snap/command.sh
+++ scripts/snap/command.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -e
-exec "$SNAP/desktop-init.sh" "$SNAP/desktop-common.sh" "$SNAP/desktop-gnome-specific.sh" "$SNAP/freeshow" "$@" --no-sandbox
+exec "$SNAP/desktop-init.sh" "$SNAP/desktop-common.sh" "$SNAP/desktop-gnome-specific.sh" "$SNAP/FreeShow" "$@" --no-sandbox
