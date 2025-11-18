e104339f793345faf24661898bba6abf586b6fd3
Kristoffer <kristoffervassbo@gmail.com>
2025-11-18 10:20:33 +0100

    Revert IP change

--- src/frontend/components/settings/tabs/Connection.svelte
+++ src/frontend/components/settings/tabs/Connection.svelte
@@ -43,7 +43,7 @@
                 // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
                 // 'IPv4' is in Node <= 17, from 18 it's a number 4 or 6
                 const familyV4Value = typeof net.family === "string" ? "IPv4" : 4
-                if (net.family === familyV4Value && !net.internal && net.gateway) {
+                if (net.family === familyV4Value && !net.internal) {
                     if (!results[name]) results[name] = []
                     results[name].push(net.address)
                 }
