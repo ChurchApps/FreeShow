6d013671312b61372c63f6ae4e217edecb350342
Jeremy Zongker <jeremy@zongker.net>
2025-12-11 01:14:39 -0600

    Encrypting all Amazing Life content

--- src/electron/contentProviders/amazingLife/AmazingLifeProvider.ts
+++ src/electron/contentProviders/amazingLife/AmazingLifeProvider.ts
@@ -91,11 +91,9 @@ export class AmazingLifeProvider extends ContentProvider<AmazingLifeScopes, Amaz
 
     /**
      * Determines if a specific URL should be encrypted
-     * AmazingLife media should only be encrypted if it has a pingback URL (licensed content)
      */
-    shouldEncrypt(_url: string, pingbackUrl?: string): boolean {
-        // Only encrypt if this has a pingback URL (indicating licensed content)
-        return !!pingbackUrl
+    shouldEncrypt(_url: string, _pingbackUrl?: string): boolean {
+        return true;
     }
 
     /**
