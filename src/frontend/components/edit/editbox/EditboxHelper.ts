fb1ddebbbbc90a7633539a4c74a0d27a16b866d9
Kristoffer <kristoffervassbo@gmail.com>
2026-02-05 09:13:56 +0100

    Fixed editor space rendering #2778

--- src/frontend/components/edit/editbox/EditboxHelper.ts
+++ src/frontend/components/edit/editbox/EditboxHelper.ts
@@ -186,7 +186,7 @@ export class EditboxHelper {
 
                 const textStyle = a.style || listStyle ? 'style="' + this.getCustomTextStyle(a.style) + listStyle + '"' : ""
                 let value = a.value?.replaceAll("\n", "<br>") || "<br>"
-                if (value === " ") value = "&nbsp;"
+                // if (value === " ") value = "&nbsp;"
 
                 // this will "hide" any HTML tags if any in the actual text content (not chords or text editor)
                 html += `<span class="${a.customType && !a.customType.includes("jw") ? "custom" : ""}" ${plain ? "" : textStyle} data-chords='${JSON.stringify(textChords)}'>` + value + "</span>"
