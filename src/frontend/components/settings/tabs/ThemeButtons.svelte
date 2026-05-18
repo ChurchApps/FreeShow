c9bf6279304d1eed6ba35dbbbe6540f86b57c80f
Kristoffer <kristoffervassbo@gmail.com>
2026-05-18 08:46:26 +0200

    Charcoal theme #3259

--- src/frontend/components/settings/tabs/ThemeButtons.svelte
+++ src/frontend/components/settings/tabs/ThemeButtons.svelte
@@ -20,7 +20,7 @@
     }
 </script>
 
-{#if Object.values($themes).length < 10}
+{#if Object.values($themes).length < 11}
     <MaterialButton title="settings.reset_themes" icon="reset" on:click={resetThemes} />
 {/if}
 <MaterialButton title="actions.import" icon="import" on:click={importTheme} />
