79a770d522e7c3652d0e05ebcca17893e7797982
Kristoffer <kristoffervassbo@gmail.com>
2026-06-03 11:58:58 +0200

    Export multiple actions at the same time

--- src/frontend/components/context/menuClick.ts
+++ src/frontend/components/context/menuClick.ts
@@ -905,19 +905,22 @@ const clickActions = {
         }
 
         if (obj.sel?.id === "theme") {
-            const theme = get(themes)[obj.sel.data[0]?.id]
-            if (!theme) return
-            send(EXPORT, ["THEME"], { content: theme })
+            obj.sel.data.forEach(({ id }) => {
+                const theme = clone(get(themes)[id])
+                if (!theme) return
 
+                send(EXPORT, ["THEME"], { content: theme })
+            })
             return
         }
 
         if (obj.sel?.id === "action") {
-            const id = obj.sel.data[0]?.id
-            const action = get(actions)[id]
-            if (!action) return
-            send(EXPORT, ["ACTION"], { content: { ...action, id } })
+            obj.sel.data.forEach(({ id }) => {
+                const action = clone(get(actions)[id])
+                if (!action) return
 
+                send(EXPORT, ["ACTION"], { content: { ...action, id } })
+            })
             return
         }
     },
