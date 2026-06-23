31000c5e0f6885d3e2f9aeb353c995253fbac555
Kristoffer <kristoffervassbo@gmail.com>
2026-06-23 15:40:15 +0200

    Update

--- src/types/Stage.ts
+++ src/types/Stage.ts
@@ -51,9 +51,9 @@ export interface StageItem {
     // textbox
     chords?: { enabled?: boolean; color?: string; size?: number }
     auto?: boolean
-    flash?: { enabled?: boolean; color?: string; count?: number } // item background flash
     textFit?: AutosizeTypes // auto size text fix option (default: growToFit)
     button?: { press?: string; release?: string } // click actions
+    flash?: { enabled?: boolean; color?: string; count?: number } // item background flash
 
     // slide text
     slideOffset?: number
