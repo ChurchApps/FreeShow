e938eca5d71ad49dcfb802db2538dd288706b717
Kristoffer <kristoffervassbo@gmail.com>
2026-07-20 13:27:47 +0200

    Updated cloud init popup

--- src/frontend/utils/cloudSync.ts
+++ src/frontend/utils/cloudSync.ts
@@ -84,10 +84,12 @@ export async function chooseTeam(team: { id: string; churchId: string; name: str
     const existingData = await requestMain(Main.CLOUD_DATA, { id, churchId: team.churchId, teamId: team.id })
     if (existingData) {
         // ensure previous popup is closed first to prevent Svelte bug "locking" popup
+        activePopup.set(null)
         setTimeout(() => activePopup.set("cloud_method"), 250)
         return
     }
 
+    if (get(activePopup) === "cloud_sync") activePopup.set(null)
     syncWithCloud(true)
 }
 
