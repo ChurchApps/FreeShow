45a7d6c3dabb8a7663efd90d5daa2bc36af6469c
Kristoffer <kristoffervassbo@gmail.com>
2026-07-30 14:49:07 +0200

    Tweak

--- src/server/common/util/customIcons.ts
+++ src/server/common/util/customIcons.ts
@@ -8,7 +8,7 @@ export const customIcons = {
     pentagon: '<g><rect fill="none" height="24" width="24"/></g><g><polygon points="2,9 6,21 18,21 22,9 12,2"/></g>',
     hexagon: '<g><rect fill="none" height="24" width="24"/></g><g><polygon points="17.2,3 6.8,3 1.6,12 6.8,21 17.2,21 22.4,12"/></g>',
 
-    cross: '<g><rect fill="none" height="24" width="24"/></g><g><rect x="10.5" y="3" width="3" height="18" rx="0.5"/><rect x="6" y="8.5" width="12" height="3" rx="0.5"/></g>',
+    cross: '<g><rect fill="none" height="24" width="24"/></g><g><rect x="10.5" y="3" width="3" height="18" rx="0.5"/><rect x="6" y="7.6" width="12" height="3" rx="0.5"/></g>',
     anchor: '<g><rect fill="none" height="24" width="24"/><path d="M17,15l1.55,1.55c-0.96,1.69-3.33,3.04-5.55,3.37V11h3V9h-3V7.82C14.16,7.4,15,6.3,15,5c0-1.65-1.35-3-3-3S9,3.35,9,5 c0,1.3,0.84,2.4,2,2.82V9H8v2h3v8.92c-2.22-0.33-4.59-1.68-5.55-3.37L7,15l-4-3v3c0,3.88,4.92,7,9,7s9-3.12,9-7v-3L17,15z M12,4 c0.55,0,1,0.45,1,1s-0.45,1-1,1s-1-0.45-1-1S11.45,4,12,4z"/></g>',
     heart: '<path d="M0 0h24v24H0z" fill="none"/><path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>',
     star: '<g><rect fill="none" height="24" width="24" x="0"/><polygon points="14.43,10 12,2 9.57,10 2,10 8.18,14.41 5.83,22 12,17.31 18.18,22 15.83,14.41 22,10"/></g>',
