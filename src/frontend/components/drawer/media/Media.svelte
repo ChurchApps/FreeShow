94fdebea5cb9ec111e1635982cca5836cd1fd071
Gladson Sam <gladson2007@outlook.com>
2026-08-07 23:23:26 +0800

    Fix content library list view and wire up search

--- src/frontend/components/drawer/media/Media.svelte
+++ src/frontend/components/drawer/media/Media.svelte
@@ -537,7 +537,7 @@
                 <PlayerVideos active={onlineTab} {searchValue} />
             </div>
         {:else if active === "online" && onlineTab === "canva"}
-            <Canva />
+            <Canva {searchValue} />
         {:else if active === "inputs"}
             <div class="gridgap">
                 {#if inputsTab === "cameras"}
