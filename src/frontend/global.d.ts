b89a7460360dea6abaeba756b701b84c3a77d02b
Kristoffer <kristoffervassbo@gmail.com>
2026-08-21 11:10:55 +0200

    Fixed LTC timecode receive worker not loading in prod

--- src/frontend/global.d.ts
+++ src/frontend/global.d.ts
@@ -37,20 +37,9 @@ interface EventTarget {
     closest?: any
 }
 
-declare module "*?worker&url" {
-    const url: string
-    export default url
-}
-
-declare module "*?url" {
-    const url: string
-    export default url
-}
-
 declare class AudioWorkletProcessor {
     port: MessagePort
     process(inputs: Float32Array[][], outputs: Float32Array[][], parameters: Record<string, Float32Array>): boolean
 }
 
 declare function registerProcessor(name: string, processorCtor: new () => AudioWorkletProcessor): void
-
