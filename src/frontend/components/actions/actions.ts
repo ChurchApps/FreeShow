4e574583ff2e1bf2e6cbeba61857b3c3ab4fc84d
Kristoffer <kristoffervassbo@gmail.com>
2026-09-24 14:02:14 +0200

    Dropping scene on slide creates a Start scene action

--- src/frontend/components/actions/actions.ts
+++ src/frontend/components/actions/actions.ts
@@ -171,6 +171,29 @@ export function customActionActivation(id: string, specificActivation: any = nul
     }
 }
 
+export function isMatchingSlideAction(existingAction: any, triggerId: string, newActionValues: any = {}, canAddMultiple = false): boolean {
+    // different action triggers
+    const actionTriggerId = getActionTriggerId(existingAction?.triggers?.[0])
+    if (actionTriggerId !== triggerId) return false
+
+    // special handling for "start_scene" trigger: allow multiple if "bindings" are different
+    if (triggerId === "start_scene") {
+        const currentScenes = get(scenes)
+        const existingSceneId = existingAction.actionValues?.start_scene?.id
+        const newSceneId = newActionValues?.start_scene?.id ?? newActionValues?.id
+        const existingBindings = (currentScenes[existingSceneId]?.bindings || []).slice().sort()
+        const newBindings = (currentScenes[newSceneId]?.bindings || []).slice().sort()
+        return JSON.stringify(existingBindings) === JSON.stringify(newBindings)
+    }
+
+    // if action cannot have multiple instances, replace any existing
+    if (!canAddMultiple) return true
+
+    // if action can have multiple instances, only replace if values are exactly the same
+    const newValues = newActionValues?.[triggerId] !== undefined ? newActionValues : { [triggerId]: newActionValues }
+    return JSON.stringify(existingAction.actionValues) === JSON.stringify(newValues)
+}
+
 export function addSlideAction(slideIndex: number, actionId: string, actionValue: any = {}, allowMultiple = false) {
     if (slideIndex < 0) return
 
@@ -190,9 +213,9 @@ export function addSlideAction(slideIndex: number, actionId: string, actionValue
     const data = actionData[actionId]
     const canAddMultiple = data?.canAddMultiple || allowMultiple
 
-    const existingIndex = slideActions.slideActions.findIndex((a) => a.triggers?.[0] === actionId)
-    if (canAddMultiple || existingIndex < 0) slideActions.slideActions.push(action)
-    else slideActions.slideActions[existingIndex] = action
+    const existingIndex = slideActions.slideActions.findIndex((a) => isMatchingSlideAction(a, actionId, actionValues, canAddMultiple))
+    if (existingIndex > -1) slideActions.slideActions[existingIndex] = { ...action, id: slideActions.slideActions[existingIndex].id }
+    else slideActions.slideActions.push(action)
 
     history({ id: "SHOW_LAYOUT", newData: { key: "actions", data: slideActions, indexes: [slideIndex] } })
 }
--- src/frontend/components/helpers/drop.ts
+++ src/frontend/components/helpers/drop.ts
@@ -15,7 +15,7 @@ export type DropAreas = "all_slides" | "slides" | "slide" | "edit" | "shows" | "
 
 const areas = {
     all_slides: ["template"],
-    slides: ["media", "player", "urls", "audio", "audio_effect", "overlay", "sound", "effect", "screen", "ndi", "camera", "microphone", "scripture", "category_audio", "audio_stream", "metronome", "show", "global_timer", "variable", "midi", "action", "omt", "blackmagic"], // group
+    slides: ["media", "player", "urls", "audio", "audio_effect", "overlay", "sound", "effect", "screen", "ndi", "camera", "microphone", "scripture", "category_audio", "audio_stream", "metronome", "show", "global_timer", "variable", "midi", "action", "omt", "blackmagic", "scene"], // group
     // slide: ["overlay", "sound", "camera"], // "media",
     // projects: ["folder"],
     project: ["show_drawer", "media", "audio", "audio_effect", "overlay", "player", "scripture", "effect", "screen", "ndi", "camera", "omt", "blackmagic"],
